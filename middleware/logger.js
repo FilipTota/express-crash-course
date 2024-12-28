2b970c8a5527b03a510a367b45be5690d6efc2e8
filiptota <filip.tota5@gmail.com>
2024-12-28 13:13:35 +0100

    external library example

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,7 +1,19 @@
+import colors from "colors";
+
 const logger = (req, res, next) => {
   // we can implement this on a route or the app level
+
+  const methodColors = {
+    GET: "green",
+    POST: "yellow",
+    PUT: "blue",
+    DELETE: "red",
+  };
+  const color = methodColors[req.method] || "white";
+
   console.log(
-    `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`
+    `${req.method}`[color] +
+      ` ${req.protocol}://${req.get("host")}${req.originalUrl}`
   );
   next();
 };
